43c754ef8f37e7b9e772379fc004c2e9b1ce88d4
Subrata Das <subratappt@gmail.com>
2022-04-22 12:25:01 +0530

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/ohmybash/oh-my-bash.git ${HOME}/.oh-my-bash
-git clone https://github.com/subratappt/OMB.git ${HOME}/.oh-my-bash/custom
+git clone https://github.com/subratappt/OMB.git ${HOME}/.oh-my-bash/custom -f
 mv ${HOME}/.bashrc ${HOME}/.bashrc_backup
-cp ${HOME}/.omb/bashrc.template.sh ${HOME}/.bashrc
+cp ${HOME}/.oh-my-bash/custom/bashrc.template.sh ${HOME}/.bashrc
 source ${HOME}/.bashrc
